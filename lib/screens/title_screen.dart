b0ab4b25ce74098ad5f1b37b5535652553d3e712
Jason Park <jason808@naver.com>
2024-01-08 17:38:49 +0900

    title_screen 디자인 완성

--- lib/screens/title_screen.dart
+++ lib/screens/title_screen.dart
@@ -1,9 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class TitleScreen extends StatelessWidget {
   const TitleScreen({super.key});
   @override
   Widget build(context) {
-    return Text("Start Screen");
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          'lib/assets/images/logo/dog.png',
+          height: 300,
+        ),
+        const SizedBox(height: 60),
+        Text(
+          "개잘알 테스트",
+          style: GoogleFonts.nanumGothic(
+              fontSize: 55, fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(height: 40),
+        ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 5, horizontal: 40)),
+            child: Text(
+              "시작",
+              style: GoogleFonts.nanumGothic(
+                  fontSize: 40,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black),
+            ))
+      ],
+    );
   }
 }
